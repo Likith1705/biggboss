87906b768f7ca52c943210d36b434cc58e25e57a
Ubuntu <ubuntu@ip-172-31-40-12.ap-south-1.compute.internal>
2022-09-24 15:56:39 +0000

    mail1 is added

---
+++ mail1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+perc=` df -h . | awk -F " " 'NR==2{print $5}'| sed 's/%/ /g'`
+if [ $perc -ge 25 ];then
+	echo "memory reached threshold value" | mail ~s "Disk usage" likithbs703@gmail.com
+fi
